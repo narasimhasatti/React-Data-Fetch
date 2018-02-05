6e61c219be456162b93d7c611634e86d5f809e02
narasimhasatti <narasimhasatti@users.noreply.github.com>
2018-02-05 12:06:28 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,54 +10,61 @@ class App extends Component {
             datas: []
         };
         this.loadData = this.loadData.bind(this);
-  
+
     }
     loadData(url) {
         fetch(url)
             .then(response => {
                 return response.json();
             }).then(json => {
-               this.setState({
+                this.setState({
                     datas: json.films.film
                 });
             }).catch(err => {
                 console.log(err)
             })
     }
-
- componentWillMount() {
+    componentWillMount() {
         this.loadData("http://www.snagfilms.com/apis/films.json?limit=10");
     }
-      render() {
-        
+    render() {
+
         return (
-            
-            <div className = "App">
-            <div className = "container">
-            <div className = "panel-group">
+
+            <
+            div className = "App" >
+            <
+            div className = "container" >
+            <
+            div className = "panel-group" >
 
 
             {
                 this.state.datas.map((datas, dataid) => {
-                        return ( 
-                          <div class="grid-container outline">
-                            <div class="row">
-                            <div className = "col-3 col-1" key = {dataid }  >
-                            Film ID: {datas.id }
-                            </div>
-                             <div className = "col-3 col-1" key = {dataid }  >
-                            Film Title: {datas.title} 
-                            </div>
-                             </div>
-                              </div>
-                           )
-                        })
-                }
+                    return ( <
+                        div class = "grid-container outline" >
+                        <
+                        div class = "row" >
+                        <
+                        div className = "col-3 col-1"
+                        key = { dataid } >
+                        Film ID: { datas.id } <
+                        /div> <
+                        div className = "col-3 col-1"
+                        key = { dataid } >
+                        Film Title: { datas.title } <
+                        /div> <
+                        /div> <
+                        /div>
+                    )
+                })
+            }
 
-                </div> </div> </div>
+            <
+            /div> </div > < /div>
 
-            );
-        }
+        );
+    }
 
-      }
+}
 export default App;
